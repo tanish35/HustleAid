d5f3248e02d73f1e64f3d864100db0024ef53538
tanish35 <tanishmajumdar2912@gmail.com>
2025-01-28 01:51:57 +0530

    Configured shadcn,wagmi in frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,22 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
+import path from "path"; // Add path import
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 5174,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+});
